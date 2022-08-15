afda5e7598e8b1c17727e74ca76463c9a6272f9a
Alessandro "Ollie" D'Amico <ollie.damico@gmail.com>
2022-08-15 12:45:33 -0700

    adding edits to epoching.py

--- piepy/epoching.py
+++ piepy/epoching.py
@@ -14,14 +14,46 @@ import mne_bids
 from mne_bids import stats
 
 import matplotlib.pyplot as plt
-# allows for interactive window
-%matplotlib qt 
 
-def epoch(data, events_array):
+def epoch(raw, events_array=None, event_type='task', n_ms_windows=None, metadata=None):
 
     """ Epoch continuous data into events (trials if task data, n-second epochs if resting data)
     
-        data: MNE raw object, either preprocessed already or not
+        raw: MNE Raw object, either preprocessed already or not
+        
+        events_array: MNE-generated array of shape 3x[n_events], where the first column is the 
+                      value of the event, the middle column is something (?), the last column 
+                      is the timestamp of the event.
+                      
+                      If None, or if event_type =='rest', then an events array will be constructed using the n_ms_windows param such that 
+                      the continuous data are sliced into evenly-spaced chunks.
+                      
+        event_type: str, option either 'task' or 'rest', this is only necessary so as to decide whether to use events_array to epoch or create our own events array
+                      
+        n_ms_wins: the width of the time windows to slice the data into, if it is un-event related data (e.g., resting state). Must be evenly divisible by the length of the timeseries 
+                    
+        metadata: string of filepath to behavioral logs metadata, in the form of CSV files; contains trial info. Per MNE requirements, the length of metadata must match n_epochs 
 
     
     """
+    
+    if event_type=='rest':
+        events
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
