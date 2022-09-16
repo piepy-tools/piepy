14905b7ad7dc451333848454a16782152537fbbb
Dillan Cellier <dcellier@ucsd.edu>
2022-09-16 10:43:01 -0700

    adding_data_testing

--- piepy/epoching.py
+++ piepy/epoching.py
@@ -2,6 +2,13 @@
 ## import data that Sydney uploaded to google drive using BIDSpath
 
 
+#####################
+#                   #
+#   GIT PULL FIRST  #
+#                   #
+#####################
+
+
 # imports
 import os
 import numpy as np
@@ -15,7 +22,7 @@ from mne_bids import stats
 
 import matplotlib.pyplot as plt
 
-def epoch(raw, events_array=None, event_type='task', n_ms_windows=None, metadata=None):
+def epoch(raw, events_array=None, event_type='task', n_windows=None, metadata=None):
 
     ## Syd's note: we should make the epochs optional, so when integrating with the main preprocessing function, set epoching == True or False
 
@@ -23,7 +30,7 @@ def epoch(raw, events_array=None, event_type='task', n_ms_windows=None, metadata
     ##     -- The checking of csv data compatibility before data entry
     ##     -- For resting-state data, create the epochs events array for you
     ##     -- cleaning epochs automatically ?
-    ##     -- If there's not an event
+    ##     -- If there's not an events_array from the BIDS object, or because it's resting state data, try creating one
 
     """ Epoch continuous data into events (trials if task data, n-second epochs if resting data)
     
@@ -38,7 +45,7 @@ def epoch(raw, events_array=None, event_type='task', n_ms_windows=None, metadata
                       
         event_type: str, option either 'task' or 'rest', this is only necessary so as to decide whether to use events_array to epoch or create our own events array
                       
-        n_ms_wins: the width of the time windows to slice the data into, if it is un-event related data (e.g., resting state). Must be evenly divisible by the length of the timeseries 
+        n_windows: the width of the time windows to slice the data into, if it is un-event related data (e.g., resting state). Must be evenly divisible by the length of the timeseries 
                     
         metadata: string of filepath to behavioral logs metadata, in the form of CSV files; contains trial info. Per MNE requirements, the length of metadata must match n_epochs 
 
@@ -53,11 +60,26 @@ def epoch(raw, events_array=None, event_type='task', n_ms_windows=None, metadata
 
 
     """
-=======
-    """
+
+    raw.load_data()
     
     if event_type=='rest':
-        events
+        # if the events type is rest, then the events array is going to just chunk the data up into arbitrary windows that are divisible with the length of the data
+
+        n_times = len(raw.times)
+
+        try:
+            assert ((n_times%n_windows) > 0), 'the length of data must be evenly divisible by n_windows'
+        except AssertionError as msg:
+            print(msg)
+
+
+        events_array = np.array()
+
+        
+
+
+    
     
     
     
